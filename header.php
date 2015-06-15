a133899ea1d9adaa6439eca1de023bd7890e33f7
Amy Dutton <amy@ahhacreative.com>
2015-06-15 10:59:05 -0500

    Updated header
    * Removed Apple Icon (might need to add this back in)
    * Aded comments for adding webfonts
    * Removed conditionzr

--- header.php
+++ header.php
@@ -6,21 +6,16 @@
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
+		<!-- TYPEKIT / GOOGLE FONTS / FONTS.COM -->
+
+		<!-- STYLES ARE ENQUED THROUGH FUNCTIONS.PHP -->
+
 		<?php wp_head(); ?>
-		<script>
-        // conditionizr.com
-        // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
-        </script>
 
 	</head>
 	<body <?php body_class(); ?>>
