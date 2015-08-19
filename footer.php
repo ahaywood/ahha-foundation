20e408e71203ebaab9c49499f79b6ea4d1880acc
Amy Dutton <amy@ahhacreative.com>
2015-08-19 14:20:27 -0500

    Removed default footer
    Was having to re-write too often

--- footer.php
+++ footer.php
@@ -1,12 +1,3 @@
-		<footer>
-			<div class="row">
-				<div class="large-6 medium-6 columns">
-					<div id="copyright">Copyright &copy; <?= date('Y'); ?> <?php bloginfo('name'); ?> All Rights Reserved.</div>
-				</div>
-				<div class="large-6 medium-6 columns">Handcrafted by <a href="http://ahhacreative.com" title="Ah Ha Creative">Ah Ha Creative</a>.</div>
-			</div>
-		</footer>
-
 		<?php wp_footer(); ?>
 
 		<!-- TODO: ADD GOOGLE ANALYTICS -->
