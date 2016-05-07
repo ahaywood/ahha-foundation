561e5c4f8e60e64a08b6926f45911e7cc0050c61
Amy Dutton <amy@ahhacreative.com>
2016-05-07 08:41:40 -0500

    Removed description meta field so that the Yoast Plugin will work

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 		<!-- TYPEKIT / GOOGLE FONTS / FONTS.COM -->
 
